9593133112bc610b5f572e9d1448016d2790b11e
michaelcordell95 <michaelcordell95@gmail.com>
2026-03-09 20:30:50 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // TypeScripture Service Worker
 // Caches key files for offline use
 
-const CACHE_NAME = 'typescripture-v39';
+const CACHE_NAME = 'typescripture-v40';
 
 const PRECACHE_FILES = [
   '/',
